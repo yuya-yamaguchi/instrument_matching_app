147bf8646751802f5df116fd1e5b8b646eaccc66
yuya-yamaguchi <nsy1023n@gmail.com>
2020-11-24 16:47:34 +0900

    ログイン機能実装のためapiサーバにdevise_tokeb_auth導入

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  # mount_devise_token_auth_for 'User', at: 'auth'
   match '*path' => 'options_request#preflight', via: :options
 
+  
+
   namespace :api, { format: 'json' } do
+    namespace :v1 do
+      mount_devise_token_auth_for "User", at: "auth"
+    end
+
     resources :boards, only: [:index, :create]
   end
 end
